203abe4fdbe0c35ec26192cb48590176a7f07adb
Matt0107 <mathieu.durimel@gmail.com>
2023-01-25 15:25:45 +0100

    move ball

--- ball.js
+++ ball.js
@@ -1,12 +1,16 @@
 class Ball {
-  constructor() {
+  constructor(image) {
+    this.image = image;
     this.width = 20;
     this.height = 20;
     this.x = 400;
     this.y = 60;
-    this.image;
+    this.velocity = 5;
+    // this.ballX = Math(random) * 3;
   }
   draw() {
+    // this.x += this.ballX;
+    this.y += this.velocity;
     image(this.image, this.x, this.y, this.width, this.height);
   }
 }
